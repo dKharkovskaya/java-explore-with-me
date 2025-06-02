4dd150c4951e018b295b55046cdaf8981670f8b0
Daria Kharkovskaya <dicsi59@gmail.com>
2025-06-02 23:36:42 +0300

    исправления в классе dto

--- stats-service/stat-dto/src/main/java/ru/practicum/explore/StatsDtoInput.java
+++ stats-service/stat-dto/src/main/java/ru/practicum/explore/StatsDtoInput.java
@@ -1,47 +1,15 @@
 package ru.practicum.explore;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
 @Data
+@Getter
+@Setter
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class StatsDtoInput {
-
     private String app;
     private String uri;
     private String ip;
     private String timestamp;
-
-
-    public String getApp() {
-        return app;
-    }
-
-    public void setApp(String app) {
-        this.app = app;
-    }
-
-    public String getUri() {
-        return uri;
-    }
-
-    public void setUri(String uri) {
-        this.uri = uri;
-    }
-
-    public String getIp() {
-        return ip;
-    }
-
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
 }
