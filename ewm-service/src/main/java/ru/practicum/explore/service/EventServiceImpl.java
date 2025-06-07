e7d89daf9acc03f5d43827bb1ec08fe2671d8853
Daria Kharkovskaya <dicsi59@gmail.com>
2025-06-07 15:05:59 +0300

    исправление в стилях

--- ewm-service/src/main/java/ru/practicum/explore/service/EventServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/explore/service/EventServiceImpl.java
@@ -21,7 +21,6 @@ import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
