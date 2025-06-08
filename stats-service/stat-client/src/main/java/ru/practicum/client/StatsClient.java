738a5ce387407221886080a1cbf3924a406de6b7
Daria Kharkovskaya <dicsi59@gmail.com>
2025-06-08 22:05:24 +0300

    исправление в коде

--- stats-service/stat-client/src/main/java/ru/practicum/client/StatsClient.java
+++ stats-service/stat-client/src/main/java/ru/practicum/client/StatsClient.java
@@ -3,7 +3,6 @@ package ru.practicum.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ru.practicum.explore.StatsDtoInput;
