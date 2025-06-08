a75dca151878e8b48829ca592399b664d4828160
Daria Kharkovskaya <dicsi59@gmail.com>
2025-06-08 15:47:19 +0300

    исправление в коде

--- ewm-service/src/main/java/ru/practicum/explore/repository/UserRepository.java
+++ ewm-service/src/main/java/ru/practicum/explore/repository/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.explore.model.User;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findAllByIdIn(List<Long> ids);
