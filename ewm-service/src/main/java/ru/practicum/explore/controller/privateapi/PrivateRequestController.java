b434b5f8ac57f8f57c11630875088e03bfd9a162
Daria Kharkovskaya <dicsi59@gmail.com>
2025-06-12 11:58:42 +0300

    исправление в коде

--- ewm-service/src/main/java/ru/practicum/explore/controller/privateapi/PrivateRequestController.java
+++ ewm-service/src/main/java/ru/practicum/explore/controller/privateapi/PrivateRequestController.java
@@ -4,10 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.explore.dto.request.EventRequestStatusUpdateRequest;
+import ru.practicum.explore.dto.request.EventRequestStatusUpdateResult;
 import ru.practicum.explore.dto.request.ParticipationRequestDto;
 import ru.practicum.explore.service.RequestService;
 
-import javax.validation.constraints.Positive;
 import java.util.List;
 
 @RestController
@@ -19,22 +20,19 @@ public class PrivateRequestController {
     private final RequestService requestService;
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
     public List<ParticipationRequestDto> getUserRequests(@PathVariable Long userId) {
         return requestService.getUserRequests(userId);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ParticipationRequestDto addRequest(@PathVariable @Positive Long userId,
-                                              @RequestParam @Positive Long eventId) {
+    public ParticipationRequestDto addRequest(@PathVariable Long userId, @RequestParam Long eventId) {
         return requestService.createRequest(userId, eventId);
     }
 
     @PatchMapping("/{requestId}/cancel")
-    @ResponseStatus(HttpStatus.OK)
-    public ParticipationRequestDto cancelRequest(@PathVariable @Positive Long userId,
-                                                 @PathVariable @Positive Long requestId) {
+    public ParticipationRequestDto cancelRequest(@PathVariable Long userId, @PathVariable Long requestId) {
         return requestService.cancelRequest(userId, requestId);
     }
+
 }
