16b5b4bfd7d05ceeabffb917ca8bdf8bb0a7ea62
Daria Kharkovskaya <dicsi59@gmail.com>
2025-06-12 11:12:02 +0300

    исправление в cтилях

--- ewm-service/src/main/java/ru/practicum/explore/repository/EventRepository.java
+++ ewm-service/src/main/java/ru/practicum/explore/repository/EventRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.explore.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.practicum.explore.enums.RequestState;
 import ru.practicum.explore.model.Event;
 
 import java.util.List;
