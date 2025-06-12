68edf5e6183e7ba2899d19012cddf558960beb3d
Daria Kharkovskaya <dicsi59@gmail.com>
2025-06-12 12:19:54 +0300

    исправление в коде

--- ewm-service/src/main/java/ru/practicum/explore/repository/RequestRepository.java
+++ ewm-service/src/main/java/ru/practicum/explore/repository/RequestRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.explore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.practicum.explore.enums.RequestState;
 import ru.practicum.explore.model.Request;
 
 import java.util.List;
