3004da06f9084ba1c568ee01d63de5a8045938ee
Daria Kharkovskaya <dicsi59@gmail.com>
2025-06-15 23:49:33 +0300

    Добавление ссылки на пул-реквест

--- ewm-service/src/main/java/ru/practicum/explore/controller/privateapi/PrivateCommentController.java
+++ ewm-service/src/main/java/ru/practicum/explore/controller/privateapi/PrivateCommentController.java
@@ -6,9 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.explore.dto.comment.CommentInputDto;
 import ru.practicum.explore.dto.comment.CommentShortDto;
-import ru.practicum.explore.error.exception.NotFoundException;
-import ru.practicum.explore.model.Comment;
-import ru.practicum.explore.model.User;
 import ru.practicum.explore.service.CommentService;
 
 @RequestMapping("/users/{userId}/comments")
